34f07de55f57937656a045601018cac638e2d6e3
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2026-01-29 11:17:50 +0900

    project setting done

---
+++ src/main/java/com/sparta/spartaminiproject/SpartaMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spartaminiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaMiniProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaMiniProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/spartaminiproject/SpartaMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spartaminiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaMiniProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
